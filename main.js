4cc1e51e7c9bf06d211b2e61f9771a9ff461efe7
Chlo06 <fernandez.chloe06@gmail.com>
2020-12-17 10:27:23 +0100

    correction

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const form1 = document.querySelector('#myForm').addEventListener("save",(e)=>{
+const form1 = document.querySelector('#myForm').addEventListener("submit",(e)=>{
     e.preventDefault();
-    const staff = [];
-    const registrationNumber = document.getElementById("clientNumber").value;
-    const contrat = document.getElementById("contrat").value;
-    const firstName = document.getElementById("firstName").value;
-    const lastName = document.getElementById("lastName").value;
-    const qualification = document.getElementById("qualification").value;
-    const salary = document.getElementById("salary").value;
-    const hiringDate = document.getElementById("hiringDate").value;
-    const endContratDate = document.getElementById("endContratDate").value;
+    const staff = localStorage.getItem('staff') ? localStorage.getItem('staff') : []
+    const registrationNumber = document.querySelector("#registrationNumber").value;
+    const contrat = document.querySelector("#contrat").value;
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const qualification = document.querySelector("#qualification").value;
+    const salary = document.querySelector("#salary").value;
+    const hiringDate = document.querySelector("#hiringDate").value;
+    const endContratDate = document.querySelector("#endContratDate").value;
     
 
     staff.push ({
@@ -22,14 +22,14 @@ const form1 = document.querySelector('#myForm').addEventListener("save",(e)=>{
         endContratDate,
     })
 
-    console.log(registrationNumber)
-    console.log(contrat)
-    console.log(firstName)
-    console.log(lastName)
-    console.log(qualification)
-    console.log(salary)
-    console.log(hiringDate)
-    console.log(endContratDate)
+    //console.log(registrationNumber)
+    //console.log(contrat)
+    //console.log(firstName)
+    //console.log(lastName)
+    //console.log(qualification)
+    //console.log(salary)
+    //console.log(hiringDate)
+    //console.log(endContratDate)
 
     console.log(staff)
 })
